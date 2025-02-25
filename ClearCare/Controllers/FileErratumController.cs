b69648d05c3c8dc819f92bfeffcd4dac1ab70322
Victor Tan <victortan102@gmail.com>
2025-02-25 21:12:34 +0800

    File Erratum

--- ClearCare/Controllers/FileErratumController.cs
+++ ClearCare/Controllers/FileErratumController.cs
@@ -14,7 +14,7 @@ namespace ClearCare.Controllers
         }
 
         [Route("{recordID}")]
-        public IActionResult DisplayUpdateRecord()
+        public IActionResult DisplayUpdateRecord(string recordID)
         {
             var userRole = HttpContext.Session.GetString("Role");
 
@@ -24,7 +24,39 @@ namespace ClearCare.Controllers
                 return RedirectToAction("DisplayViewRecord", "ViewRecord");
             }
 
+            ViewBag.MedicalRecordID = recordID; // Pass record ID to view
+
             return View("UpdateRecord");
+        }
+
+
+        // Form action to file erratum
+        [HttpPost]
+        [Route("FileErratumRecord")]
+        public async Task<IActionResult> FileErratum(string recordID, string erratumDetails)
+        {
+            var userRole = HttpContext.Session.GetString("Role");
+            var doctorID = HttpContext.Session.GetString("UserID");
+
+            if (userRole != "Doctor") // Only allow doctors to submit erratum
+            {
+                Console.WriteLine("You do not have permission to access this page.");
+                return RedirectToAction("DisplayViewRecord", "ViewRecord");
+            }
+
+            if (string.IsNullOrEmpty(doctorID))
+            {
+                return BadRequest("Doctor ID is missing.");
+            }
+
+            var result = await ErratumManagement.CreateErratum(recordID, erratumDetails, doctorID);
+
+            if (result == null)
+            {
+                return NotFound("Failed to create erratum.");
+            }
+
+            return RedirectToAction("DisplayViewRecord", "ViewRecord");
         }
     }
 }
