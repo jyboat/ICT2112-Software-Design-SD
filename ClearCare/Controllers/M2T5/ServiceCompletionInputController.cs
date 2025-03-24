19fa925ac11640a0ea82f831ced8479ae21624dd
Brandon Ng <65611513+Dynamis-d-Arc@users.noreply.github.com>
2025-03-24 23:48:51 +0800

    Filter
    Filter

--- ClearCare/Controllers/M2T5/ServiceCompletionInputController.cs
+++ ClearCare/Controllers/M2T5/ServiceCompletionInputController.cs
@@ -2,20 +2,9 @@ using ClearCare.Interfaces;
 using ClearCare.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using ClearCare.Models.Interface;
-
-
-
-using ClearCare.Models.DTO;
-
-
-using ClearCare.Interfaces;
-using ClearCare.Models.Entities;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClearCare.Models.Interface;
 using ClearCare.Models.DTO;
 
 namespace ClearCare.Controllers
@@ -34,15 +23,43 @@ namespace ClearCare.Controllers
         [HttpGet("appointments")]
         public async Task<IActionResult> GetAppointmentDetails(bool json = false)
         {
+            // Retrieve userId from the session
+            string userId = HttpContext.Session.GetString("UserID");
+
+            // Log or print the session value for debugging
+            Console.WriteLine($"UserId from session: {userId}");  // For debugging
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                userId = "Not logged in"; // This will let you know if the session is empty
+            }
+
+            // Fetch the appointment details
             List<ServiceAppointment> appointments = await _appointmentStatus.getAppointmentDetails();
+
+            // Map appointments to DTOs
             List<ServiceAppointmentDTO> appointmentDTOs = appointments
                 .Select(appointment => new ServiceAppointmentDTO(appointment))
                 .ToList();
 
+            // Filter the appointments to only show those that match the doctorId from session
+            if (!string.IsNullOrEmpty(userId))
+            {
+                appointmentDTOs = appointmentDTOs
+                    .Where(dto => dto.DoctorId == userId) // Only appointments for this doctor
+                    .ToList();
+            }
+
+            // Create the message to display in the view or return as part of the JSON
+            string message = $"UserId (from session): {userId} | DoctorId: {string.Join(", ", appointmentDTOs.Select(dto => dto.DoctorId).Distinct())}";
+
             if (json)
             {
-                return Json(appointmentDTOs); // Returns JSON if requested
+                return Json(new { message, appointments = appointmentDTOs }); // Return JSON with message and filtered appointments
             }
+
+            // Pass the message to the view
+            ViewBag.Message = message;
 
             // Explicitly specify the custom path for the View
             return View("~/Views/M2T5/ServiceCompletion.cshtml", appointmentDTOs);
@@ -64,6 +81,3 @@ namespace ClearCare.Controllers
         }
     }
 }
-
-
-
