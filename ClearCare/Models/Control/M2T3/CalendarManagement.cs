952b0ff0d68b7a65cec24b135bbfe08325cb9e77
Jun Yu <junyu_0611@hotmail.com>
2025-04-03 13:15:48 +0800

    Update CalendarManagement.cs
    remove comments

--- ClearCare/Models/Control/M2T3/CalendarManagement.cs
+++ ClearCare/Models/Control/M2T3/CalendarManagement.cs
@@ -7,12 +7,6 @@ using System.Threading.Tasks;
 using ClearCare.Models.Entities;
 using System.Text.Json;
 
-// TODO: 
-// +getAppointmentsByPatient(patientId: Int): <List> ServiceAppointment
-// +getAppointmentsByNurse(nurseId: Int): <List> ServiceAppointment
-// +getAppointmentsByType(serviceType: String): <List> ServiceAppointment
-// +getAppointmentsByDateRange(startDate: DateTime, endDate: DateTime): <List> ServiceAppointment
-
 namespace ClearCare.Models.Control
 {
     public class CalendarManagement
