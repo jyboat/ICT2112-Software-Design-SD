763ede3321f0c407a3c806cc3468b396791e4280
Jun Yu <junyu_0611@hotmail.com>
2025-04-05 05:59:44 +0800

    Update IServiceBacklogDB_Receive.cs

--- ClearCare/Models/Interface/M2T3/IServiceBacklogDB_Receive.cs
+++ ClearCare/Models/Interface/M2T3/IServiceBacklogDB_Receive.cs
@@ -1,6 +1,3 @@
-// defines the callback methods for receiving results or status updates from the database operations
-// implemented by NurseAvailabilityManagement; used by NurseAvailabilityGateway
-
 using System.Collections.Generic;
 using ClearCare.Models.Entities;
 using System.Threading.Tasks;
@@ -12,7 +9,6 @@ namespace ClearCare.Interfaces
         Task receiveBacklogList(List<Dictionary<string, string>> backlogList);
         Task receiveBacklogDetails(Dictionary<string, string> serviceBacklog);
         Task receiveAddStatus(string status);
-
         Task receiveDeleteStatus(string status);
     }
 }
