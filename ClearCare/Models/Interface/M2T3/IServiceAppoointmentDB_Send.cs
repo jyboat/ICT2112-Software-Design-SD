3101e38793bb51a54c7d073514c12f86a4e39914
Jun Yu <junyu_0611@hotmail.com>
2025-04-05 05:59:06 +0800

    Update IServiceAppoointmentDB_Send.cs

--- ClearCare/Models/Interface/M2T3/IServiceAppoointmentDB_Send.cs
+++ ClearCare/Models/Interface/M2T3/IServiceAppoointmentDB_Send.cs
@@ -1,6 +1,3 @@
-// defines the methods for sending (or initiating) database operations such as fetching, creating, updating, and deleting nurse availabilities.
-// implemented by NurseAvailabilityGateway; used by NurseAvailabilityManagement 
-
 using ClearCare.Models.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,17 +6,14 @@ using ClearCare.DataSource;
 namespace ClearCare.Interfaces
 {
     public interface IServiceAppointmentDB_Send
-   {
-    IServiceAppointmentDB_Receive Receiver { get; set; }
-    // fetch all service appointments from db, implemented by gateway
-    Task<List<ServiceAppointment>> fetchAllServiceAppointments();
-    Task<ServiceAppointment> fetchServiceAppointmentByID(string appointmentId); 
-    Task<string> createAppointment(ServiceAppointment appointment);
-    Task<bool> updateAppointment(ServiceAppointment appointment);
-    Task<bool> deleteAppointment (string appointmentId);
-    Task<DateTime?> fetchAppointmentTime(string appointmentId);
-    
-    Task<(List<Dictionary<string, object>> appointments, Dictionary<string, string> patientNames)> fetchAllUnscheduledPatients();
-
-   }
+    {
+        IServiceAppointmentDB_Receive Receiver { get; set; }
+        Task<List<ServiceAppointment>> fetchAllServiceAppointments();
+        Task<ServiceAppointment> fetchServiceAppointmentByID(string appointmentId);
+        Task<string> createAppointment(ServiceAppointment appointment);
+        Task<bool> updateAppointment(ServiceAppointment appointment);
+        Task<bool> deleteAppointment(string appointmentId);
+        Task<DateTime?> fetchAppointmentTime(string appointmentId);
+        Task<(List<Dictionary<string, object>> appointments, Dictionary<string, string> patientNames)> fetchAllUnscheduledPatients();
+    }
 }
