360ba39ee16909f102073f78d3338e2eebe646fc
Jun Yu <junyu_0611@hotmail.com>
2025-04-05 05:55:34 +0800

    Update IRescheduleAppointment.cs

--- ClearCare/Models/Interface/M2T3/IRescheduleAppointment.cs
+++ ClearCare/Models/Interface/M2T3/IRescheduleAppointment.cs
@@ -1,7 +1,3 @@
-// implemented by Service Appointment (M2T3); 
-// used by Manual Appointment Management (M2T3)
-// used by Automatic Appointment Management (M2T3)
-
 using System.Collections.Generic;
 using ClearCare.Models.Entities;
 using System.Threading.Tasks;
@@ -11,7 +7,15 @@ namespace ClearCare.Interfaces
 {
     public interface IRescheduleAppointment
     {
-    Task<bool> rescheduleAppointment(string appointmentId, string patientId, string nurseId,
-            string doctorId, string Service, string status, DateTime dateTime, int slot, string location);
+        Task<bool> rescheduleAppointment(
+            string appointmentId,
+            string patientId,
+            string nurseId,
+            string doctorId,
+            string Service,
+            string status,
+            DateTime dateTime,
+            int slot,
+            string location);
     }
 }
