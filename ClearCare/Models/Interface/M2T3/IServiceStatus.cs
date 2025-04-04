1fa7c66559f263bf4d660de0ba64ea64b2cfa092
Jun Yu <junyu_0611@hotmail.com>
2025-04-05 06:01:58 +0800

    Update IServiceStatus.cs

--- ClearCare/Models/Interface/M2T3/IServiceStatus.cs
+++ ClearCare/Models/Interface/M2T3/IServiceStatus.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace ClearCare.Interfaces
 {
-    public interface IServiceStatus 
+    public interface IServiceStatus
     {
         Task<List<ServiceAppointment>> retrieveAllAppointments();
         Task<ServiceAppointment> getAppointmentByID(string appointmentId);
