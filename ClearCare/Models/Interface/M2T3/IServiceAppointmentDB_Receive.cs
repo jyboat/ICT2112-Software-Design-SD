f84b5fea9c900458b1e56c6b8eb2db703214e02e
Jun Yu <junyu_0611@hotmail.com>
2025-04-05 05:58:19 +0800

    Update IServiceAppointmentDB_Receive.cs

--- ClearCare/Models/Interface/M2T3/IServiceAppointmentDB_Receive.cs
+++ ClearCare/Models/Interface/M2T3/IServiceAppointmentDB_Receive.cs
@@ -1,6 +1,3 @@
-// defines the methods for sending (or initiating) database operations such as fetching, creating, updating, and deleting nurse availabilities.
-// implemented by NurseAvailabilityGateway; used by NurseAvailabilityManagement 
-
 using ClearCare.Models.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,12 +5,12 @@ using System.Threading.Tasks;
 namespace ClearCare.Interfaces
 {
     public interface IServiceAppointmentDB_Receive
-   {
-    Task receiveServiceAppointmentList(List<ServiceAppointment> allServiceAppointments);
-    Task receiveServiceAppointmentById(ServiceAppointment serviceAppointment) ;
-    Task receiveCreatedServiceAppointmentId(string serviceAppointmentId); 
-    Task receiveUpdatedServiceAppointmentStatus(bool updateStatus);
-    Task receiveDeletedServiceAppointmentStatus(bool deleteStatus);
-    Task receiveServiceAppointmentTimeById(DateTime? dateTime);
-   }
+    {
+        Task receiveServiceAppointmentList(List<ServiceAppointment> allServiceAppointments);
+        Task receiveServiceAppointmentById(ServiceAppointment serviceAppointment);
+        Task receiveCreatedServiceAppointmentId(string serviceAppointmentId);
+        Task receiveUpdatedServiceAppointmentStatus(bool updateStatus);
+        Task receiveDeletedServiceAppointmentStatus(bool deleteStatus);
+        Task receiveServiceAppointmentTimeById(DateTime? dateTime);
+    }
 }
